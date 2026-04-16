96c8c994f7810b1ade5e29094f65f5bc136bd05a
Ryan Avery <info@chancesend.com>
2026-04-15 18:45:31 -0700

    Log visualization scaffold

--- src/log.cpp
+++ src/log.cpp
@@ -3,13 +3,14 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-#include <HardwareSerial.h>
+#include <Arduino.h>
 
 namespace Scarfnet
 {
 
 void log( const char *msg , ... )
 {
+   printf("[T+%lu] ", (unsigned long)millis());
    va_list args;
    va_start( args, msg );
    vprintf( msg, args );
